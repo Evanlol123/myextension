f09c54151e495a8e0a5dd91c71afdf3fec1a9445
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-04-14 22:47:10 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,5 @@
-
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: 'toggleExtensions', disabled: document.getElementById('extensionToggle').checked});
+  });
+});
